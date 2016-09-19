7bc8d44a710b17d56c73e2e134ea4d5125047c77
Anis ruby <aniskillerz@gmail.com>
2016-09-20 04:03:51 +0800

    anis-updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919085303) do
+ActiveRecord::Schema.define(version: 20160919195334) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -34,8 +34,9 @@ ActiveRecord::Schema.define(version: 20160919085303) do
     t.text     "body"
     t.integer  "conversation_id"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "read",            default: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
