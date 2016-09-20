c734a520dc5f8b333cbc113ef896c1650868d502
Kim Sara <sarahazizan@localhost.my>
2016-09-20 13:38:19 +0800

    edit devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 layout :layout_by_resource
 before_action :authenticate_user!
+before_action :configure_permitted_parameters, 
+if: :devise_controller? 
+protected 
+def configure_permitted_parameters 
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:gender, :birthday]) 
+end 
+
 
   protected
 
