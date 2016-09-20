37cca09fb1d1fa033da7dc2e339348f06c071eec
wanazleen <azleenfauzana@localhost.my>
2016-09-20 13:42:19 +0800

    latest list friend

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
   def show
   	@user = current_user
+	@users=User.all.where.not(id:current_user.id)
+    @user_friends = UserFriend.all
+	@friends= current_user.user_friends.pluck(:friend_id)
   end
 end
