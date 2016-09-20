e89175de7c0a5807cd614c9db6198956055bf1d0
Fairus <fairuszulkifli93@gmail.com>
2016-09-20 18:16:58 +0800

    update wall

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+  	 @wallposts = Wallpost.all.where(user_id: current_user.user_friends.pluck(:friend_id)<<current_user.id).order('created_at DESC')
   end
 end
--- app/models/wallpost.rb
+++ app/models/wallpost.rb
@@ -1,3 +1,4 @@
 class Wallpost < ApplicationRecord
 belongs_to :user
+has_many :comments
 end
