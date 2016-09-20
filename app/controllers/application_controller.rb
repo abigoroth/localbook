b62109d89b6371024024b9ea7e312fc6ce767774
Kim Sara <sarahazizan@localhost.my>
2016-09-20 18:07:04 +0800

    nampak la rangka dia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ before_action :configure_permitted_parameters,
 if: :devise_controller? 
 protected 
 def configure_permitted_parameters 
-	devise_parameter_sanitizer.permit(:sign_up, keys: [:gender, :birthday]) 
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :birthday]) 
+end 
+protected 
+def configure_permitted_parameters 
+	devise_parameter_sanitizer.permit(:edit_user, keys: [:name, :gender, :birthday, :about]) 
 end 
 
 
