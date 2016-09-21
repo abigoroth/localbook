2fa0304503fbfe6bf6446b8f83d4dafc744f2a82
Kim Sara <sarahazizan@localhost.my>
2016-09-21 12:12:38 +0800

    sara update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+ protect_from_forgery with: :exception
 layout :layout_by_resource
 before_action :authenticate_user!
 before_action :configure_permitted_parameters, 
@@ -7,11 +7,10 @@ if: :devise_controller?
 protected 
 def configure_permitted_parameters 
 	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :birthday]) 
+	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :birthday, :about]) 
 end 
-protected 
-def configure_permitted_parameters 
-	devise_parameter_sanitizer.permit(:edit_user, keys: [:name, :gender, :birthday, :about]) 
-end 
+
+
 
 
   protected
---
+++ db/migrate/20160921034744_add_age_and_city_and_relationship_status_to_users.rb
@@ -0,0 +1,7 @@
+class AddAgeAndCityAndRelationshipStatusToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :age, :integer
+    add_column :users, :city, :string
+    add_column :users, :relationship_status, :string
+  end
+end
